79ae54c491548e9830638f0672e324481c877ef3
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-06-22 12:25:50 +0530

    Events Propogation

--- script.js
+++ script.js
@@ -60,7 +60,35 @@ btnScroll.addEventListener('click', (e) => {
     behavior: "smooth"
   });
 
+});
+
+// Event -->
+const randomInt = (min, max) => Math.floor((Math.random() * (max - min + 1)) + min);
+
+const randomColor = () => `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log("LINK", e.target, e.currentTarget);
+
+  // // Stop Propogation
+  // e.stopPropagation();
+
 
 });
 
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log("LINKS", e.target, e.currentTarget);
+
+});
+
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+
+  this.style.backgroundColor = randomColor();
+  console.log("NAV", e.target, e.currentTarget);
+
+}, true);
 
