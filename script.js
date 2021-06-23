1c84a98f2b234a3909e5bb0dbcb668aa53ea6a22
Mridul00700 <55996869+Mridul00700@users.noreply.github.com>
2021-06-23 16:45:21 +0530

    TabComponent-button and container

--- script.js
+++ script.js
@@ -94,9 +94,28 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 // Tab Component ->
 
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (e.target.classList.contains('operations__tab')) {
+    const clicked = e.target.closest('.operations__tab');
 
 
+    // active tab --> move up
+    tabs.forEach(tab => {
+      tab.classList.remove('operations__tab--active');
+    })
+    clicked.classList.add('operations__tab--active');
 
+    // Activate the content area --> 
+    document.querySelectorAll('.operations__content').forEach(tabCon => tabCon.classList.remove('operations__content--active'))
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+  }
+
+});
 
 
 
